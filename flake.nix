eac6558b866ea66c0981136c396aaec5abe4b067
Marcel Müller <neikos@neikos.email>
2024-04-07 17:57:28 +0200

    Initial Commit
    Signed-off-by: Marcel Müller <neikos@neikos.email>

---
+++ flake.nix
@@ -0,0 +1,75 @@
+{
+  description = "The alles library";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [ (import inputs.rust-overlay) ];
+        };
+
+        rustTarget = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        craneLib = (inputs.crane.mkLib pkgs).overrideToolchain rustTarget;
+
+        tomlInfo = craneLib.crateNameFromCargoToml { cargoToml = ./Cargo.toml; };
+        inherit (tomlInfo) pname version;
+        src = ./.;
+
+        cargoArtifacts = craneLib.buildDepsOnly {
+          inherit src;
+          cargoExtraArgs = "--all-features --all";
+        };
+
+        alles = craneLib.buildPackage {
+          inherit cargoArtifacts src version;
+          cargoExtraArgs = "--all-features --all";
+        };
+
+      in
+      rec {
+        checks = {
+          inherit alles;
+
+          alles-clippy = craneLib.cargoClippy {
+            inherit cargoArtifacts src;
+            cargoExtraArgs = "--all --all-features";
+            cargoClippyExtraArgs = "-- --deny warnings";
+          };
+
+          alles-fmt = craneLib.cargoFmt {
+            inherit src;
+          };
+        };
+
+        packages.alles = alles;
+        packages.default = alles;
+
+        devShells.default = devShells.alles;
+        devShells.alles = pkgs.mkShell {
+          buildInputs = [ ];
+
+          nativeBuildInputs = [
+            rustTarget
+          ];
+        };
+      }
+    );
+}
